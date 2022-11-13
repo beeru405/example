e44e93537c90c210fb95d16a693cdd6054e0c389
abhic137 <46273637+abhic137@users.noreply.github.com>
2022-11-13 23:47:05 +0530

    Update Palindrome.java

--- src/main/java/com/training/example/JacocoExample/Palindrome.java
+++ src/main/java/com/training/example/JacocoExample/Palindrome.java
@@ -1,22 +1,47 @@
 package com.training.example.JacocoExample;
 
-public class Palindrome {
+//public class Palindrome {
 	
-	public boolean isPalindrome(String inputString) {
-	    if (inputString.length() == 0) {
-	        return true;
-	    } else {
-	    	String rev = "";
-	    	int length = inputString.length();
+	//public boolean isPalindrome(String inputString) {
+	    //if (inputString.length() == 0) {
+	       // return true;
+	    //} else {
+	    //	String rev = "";
+	    	//int length = inputString.length();
 	    	 
-	        for ( int i = length - 1; i >= 0; i-- )
-	           rev = rev + inputString.charAt(i);
+	       // for ( int i = length - 1; i >= 0; i-- )
+	       //    rev = rev + inputString.charAt(i);
 	   
-	        if (inputString.equals(rev))
-	           return true;
-	        else
-	           return false;
-	    }
-	}
+	       // if (inputString.equals(rev))
+	      //     return true;
+	       // else
+	       //    return false;
+	  //  }
+//	}
 
+//}
+
+
+
+class GFG
+{
+
+// Function that returns true if the word is found
+static boolean isWordPresent(String sentence, String word)
+{
+	// To break the sentence in words
+	String []s = sentence.split(" ");
+
+	// To temporarily store each individual word
+	for ( String temp :s)
+	{
+
+		// Comparing the current word
+		// with the word to be searched
+		if (temp.compareTo(word) == 0)
+		{
+			return true;
+		}
+	}
+	return false;
 }
