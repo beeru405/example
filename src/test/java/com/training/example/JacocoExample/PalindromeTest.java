3ffbbb428dd4f829100d4607cd1723984d3d62f5
abhic137 <46273637+abhic137@users.noreply.github.com>
2022-11-13 23:47:45 +0530

    Update PalindromeTest.java

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -4,11 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class PalindromeTest {
+//public class PalindromeTest {
 
-	@Test
-	public void whenEmptyString_thenAccept() {
-	    Palindrome palindromeTester = new Palindrome();
-	    assertTrue(palindromeTester.isPalindrome("madam"));
-	}
+	//@Test
+	//public void whenEmptyString_thenAccept() {
+	 //   Palindrome palindromeTester = new Palindrome();
+	   // assertTrue(palindromeTester.isPalindrome("madam"));
+	//}
+//}
+
+
+
+public class stringstr{
+
+
+@Test
+// Driver code
+public void checkstr()
+{
+    GFG stringtester = new GFG();
+    assertTrue (stringtester.isWordPresent("hello this is abhishek","hello"))
+	String s = "Geeks for Geeks";
+	String word = "Geeks";
+
+	
+
+}
 }
